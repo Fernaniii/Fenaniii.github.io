a22710ddffefc438d6b92bf2fbaa4a0ad0ea4f4b
Fernando Jose Magnaye <145454557+Fernaniii@users.noreply.github.com>
2024-09-19 22:30:02 +0000

    calculator refAactor

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,7 +1,7 @@
+const buttons = document.querySelectorAll('.btn');
 const display = document.getElementById('display');
-const buttons = Array.from(document.getElementsByClassName('btn'));
 
-buttons.map(button => {
+buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         const buttonText = e.target.innerText;
 
@@ -13,6 +13,22 @@ buttons.map(button => {
             }
         } else if (buttonText === 'C') {
             display.innerText = '0';
+        } else if (buttonText === '←') {
+            display.innerText = display.innerText.slice(0, -1) || '0';
+        } else if (buttonText === '√') {
+            try {
+                display.innerText = Math.sqrt(eval(display.innerText));
+            } catch {
+                display.innerText = 'Error';
+            }
+        } else if (buttonText === '±') {
+            if (display.innerText !== '0') {
+                if (display.innerText.startsWith('-')) {
+                    display.innerText = display.innerText.substring(1);
+                } else {
+                    display.innerText = '-' + display.innerText;
+                }
+            }
         } else {
             if (display.innerText === '0') {
                 display.innerText = buttonText;
